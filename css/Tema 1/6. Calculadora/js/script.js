0697a2ab0cf4916b5900ca8e013c2265f465af67
Miguel FGP <miguel_fgp@hotmail.es>
2020-10-23 23:16:12 +0200

    Calculadora terminada

--- css/Tema 1/6. Calculadora/js/script.js
+++ css/Tema 1/6. Calculadora/js/script.js
@@ -1,15 +1,60 @@
 var op1;
 var op2;
+var sign;
+var operating = false;
 
-function insertNumber($num){
-    var pantalla = document.getElementById('screen').innerHTML;
-    if (pantalla.localeCompare("0")){
-        document.getElementById('screen').innerHTML += $num;
-    } else {
-        document.getElementById('screen').innerHTML = $num;
+function insertNumber(num){
+    var screenNumber = document.getElementById('screen').innerHTML;
+
+    if (screenNumber.length < 5){
+        if (screenNumber.localeCompare("0") && !operating){
+            document.getElementById('screen').innerHTML += num;
+        } else {
+            document.getElementById('screen').innerHTML = num;
+            operating = false;
+        }
     }
 }
 
 function deleteScreen(){
     document.getElementById('screen').innerHTML = '0';
+    op1 = 0;
+    sign = "";
+}
+
+function operate(key){
+    let text;
+    operating = true;
+
+    if (!sign){
+        sign = key;
+        op1 = parseInt(document.getElementById('screen').innerHTML);
+    } else {
+        op2 = parseInt(document.getElementById('screen').innerHTML);
+        switch (sign){
+            case '+': 
+                op1 += op2;
+                break;
+            case '-': 
+                op1 -= op2; 
+                break;
+            case 'X': 
+                op1 *= op2; 
+                break;
+            case '/': 
+                op1 /= op2; 
+                break;
+        }
+        sign = key;
+
+        text = checkLength(op1);
+
+        document.getElementById('screen').innerHTML = text;
+    }
+}
+
+function checkLength(number){
+    number = number.toString();
+    number = number.substring(0, 5);
+    return number
 }
