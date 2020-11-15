b798e49dc78203c13f8c90926eb5e2e2ac36746f
Miguel FGP <miguel_fgp@hotmail.es>
2020-11-15 13:14:13 +0100

    Plantilla CSS y JS

--- css/Tema 3/Plantilla/js/script.js
+++ css/Tema 3/Plantilla/js/script.js
@@ -9,29 +9,29 @@ function randomPallette(){
     let background = 'rgb('+(r+100)+', '+(g+100)+', '+(b+100)+')';
     let column = 'rgb('+(r+50)+', '+(g+50)+', '+(b+50)+')';
 
-    
+    document.getElementById('content').style.color = font;
     document.getElementById('header').style.backgroundColor = menu;
     document.getElementById('header').style.borderColor = border;
-    //document.getElementById('footer').style.backgroundColor = menu;
-    //document.getElementById('footer').style.borderColor = border;
     document.getElementById('nav').style.backgroundColor = column;
     document.getElementById('menu').style.borderColor = border;
     document.getElementById('list').getElementsByTagName('ul')[0].getElementsByTagName('li')[0].style.borderColor = border;
     document.getElementById('list').getElementsByTagName('ul')[0].getElementsByTagName('li')[1].style.borderColor = border;
     document.getElementById('list').getElementsByTagName('ul')[0].getElementsByTagName('li')[2].style.borderColor = border;
     document.getElementById('list').getElementsByTagName('ul')[0].getElementsByTagName('li')[3].style.borderColor = border;
-    document.getElementById('list').getElementsByTagName('ul')[0].getElementsByTagName('li')[4].style.borderColor = border;
     document.getElementById('button').style.borderColor = border;
     document.getElementById('hidden-list').style.backgroundColor = column;
-    document.getElementById('hidden-list').style.borderColor = border;
+    document.getElementById('hidden-list').style.outlineColor = border;
     document.getElementById('hidden-list').getElementsByTagName('ul')[0].getElementsByTagName('li')[0].style.borderColor = border;
     document.getElementById('hidden-list').getElementsByTagName('ul')[0].getElementsByTagName('li')[1].style.borderColor = border;
     document.getElementById('hidden-list').getElementsByTagName('ul')[0].getElementsByTagName('li')[2].style.borderColor = border;
     document.getElementById('hidden-list').getElementsByTagName('ul')[0].getElementsByTagName('li')[3].style.borderColor = border;
-    document.getElementById('hidden-list').getElementsByTagName('ul')[0].getElementsByTagName('li')[4].style.borderColor = border;    
     document.getElementById('main').style.backgroundColor = background;
+    document.getElementById('main').style.borderColor = border; 
     document.getElementById('logo').style.backgroundColor = column;
-    document.getElementById('logo').style.borderColor = border;    
-    document.body.style.color = font;
-    //document.getElementById('footer').style.color = font;
+    document.getElementById('logo').style.borderColor = border;   
+    document.getElementById('aside').style.backgroundColor = column;
+    document.getElementById('aside').style.borderColor = border;  
+    document.getElementById('footer').style.backgroundColor = menu;
+    document.getElementById('footer').style.borderColor = border;
+    document.getElementById('footer').style.color = font;
 }
